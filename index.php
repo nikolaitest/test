05d48c84341041cda5aa45941bf90fc6f0d43782
Niki <mr.xfly@gmail.com>
2016-02-27 12:48:33 +0200

    afdasdf

--- index.php
+++ index.php
@@ -4,9 +4,13 @@ echo '1.0.0 -stage';
 echo '1.1.0 -stage';
 echo '1.1.1 -stage';
 echo '1.0.0 -master';
+<<<<<<< HEAD
 echo '1.0.0 -test';
 echo '1.0.1 -test';
 echo '1.2.1 -test';
 echo '1.3.1 -test';
 echo '1.4.1 -test';
+=======
+echo '1.1.0 -master';
+>>>>>>> тест1
 ?>
